53fd063529aa65c7c97ef474c6fe61f70622ee6b
Selomon1 <soya.vandy1950@gmail.com>
2023-11-22 11:47:06 -0800

    AirBnB Clone v2

--- models/review.py
+++ models/review.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""
+Defines the Review class
+"""
+
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey

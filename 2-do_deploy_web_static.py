1e5071d57d7ef098ecf1893291946c628c33ba5c
Selomon1 <soya.vandy1950@gmail.com>
2023-12-10 23:10:48 +0300

    AirBnB_clone_v2 Deploy Web Static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,17 +22,16 @@ def do_deploy(archive_path):
         ext_file = archive_name.split(".")[0]
 
         put(archive_path, "/tmp/{}".format(archive_name))
-        run("mkdir -p /data/web_static/releases/{}/".format(ext_file))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(ext_file))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
                 .format(archive_name, ext_file))
-        run("rm /tmp/{}".format(archive_name))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/"
-                .format(ext_file, ext_file))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(ext_file))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("sudo rm /tmp/{}".format(archive_name))
+        run("sudo mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(ext_file, ext_file))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(ext_file))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
                 .format(ext_file))
-        return True
     except Exception:
         return False
+    return True

06872d57434bcca8457b16330ac44ac1383f865a
Selomon1 <soya.vandy1950@gmail.com>
2023-12-10 23:01:53 +0300

    AirBnB_clone_v2 Deploy Web Static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,11 +6,9 @@ Fabric Script to distribute an archive to web servers
 
 from fabric.api import env, put, run
 from os.path import exists
-from datetime import datetime
 
 env.host = ['3.85.1.161', '100.26.178.134']
 env.user = 'ubuntu'
-env.key_file = '~/.ssh/school'
 
 def do_deploy(archive_path):
     """
@@ -20,20 +18,21 @@ def do_deploy(archive_path):
         return False
 
     try:
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_file = "web_static_{}.tgz".format(timestamp)
+        archive_name = archive_path.split("/")[-1]
+        ext_file = archive_name.split(".")[0]
 
-        put(archive_path, "/tmp/{}".format(archive_file))
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_file[:-4]))
+        put(archive_path, "/tmp/{}".format(archive_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(ext_file))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                .format(archive_file, archive_file[:-4]))
-        run("rm /tmp/{}".format(archive_file))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
-                .format(archive_file[:-4], archive_file[:-4]))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_file[:-4]))
+                .format(archive_name, ext_file))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/"
+                .format(ext_file, ext_file))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(ext_file))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                .format(archive_file[:-4]))
+                .format(ext_file))
         return True
     except Exception:
         return False

7b8d11c73f0b6e92b27e9a6878c8491f11237e41
root <Michaelelsa12@gmail.com>
2023-12-24 09:07:16 +0000

    Web-Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
 from flask import Flask
+#Flask Application
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def Hbnb(name):
-    return “Hello HBNB!”
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':

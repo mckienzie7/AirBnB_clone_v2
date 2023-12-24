a6259a45ec49bd63b339b64a5811407f8db70ee9
root <michaelelsa12@gmail.com>
2023-12-24 10:29:10 +0000

    web-Flask(3)

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,6 +31,7 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def pyhton(text="is cool"):
     """Displays 'Python' followed by the value of <text>."""

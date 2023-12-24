95f831ff58be0261a4241e490f908b85151b6599
Michael Solomon <miki@Mickey>
2023-12-24 11:59:23 +0300

    web-Flask-1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hbnb(name):
+    return “Hello HBNB!”
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
